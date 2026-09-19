ae0a4f7d255707a9d08f40124089ea9fe288f72b
Lifestory8000 <3550936496@qq.com>
2026-09-19 16:17:44 +0800

    node

---
+++ node.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct node{
+    int data;
+    struct node *next;
+}node;
+int main(void){
+node *n1 = (node *)malloc(sizeof(node));
+node *p=n1;
+p->data=2;
+p->next=NULL;
+node *head=n1;
+printf("data=\n");
+while(head!=NULL){
+printf("%d", head->data);
+node *temp=head;
+head=head->next;
+free(temp);
+}
+
+
+    return 0;
+}
